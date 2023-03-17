0a4cd1033b77689e363a0ad7d379ad2d7fa5f019
numangit <abdulnumanjob@gmail.com>
2023-03-17 17:05:52 +0600

    book filter component added

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import BookContainer from '../components/home/BookContainer';
+import BookFilter from '../components/home/BookFilter';
 
 const Home = () => {
     return (
         <main class="py-12 px-6 2xl:px-6 container">
             <div class="order-2 xl:-order-1">
-                <div class="flex items-center justify-between mb-12">
-                    <h4 class="mt-2 text-xl font-bold">Book List</h4>
-
-                    <div class="flex items-center space-x-4">
-                        <button class="lws-filter-btn active-filter">All</button>
-                        <button class="lws-filter-btn">Featured</button>
-                    </div>
-                </div>
+                <BookFilter />
                 <BookContainer />
             </div>
         </main>
